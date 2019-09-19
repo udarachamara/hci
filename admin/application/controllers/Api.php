0b161afaa4423a33de179af7c8027912146eb8b8
udara chamara <udara@accusys.lk>
2019-09-19 22:09:53 +0530

    update api 2019-09-19 product search

--- admin/application/controllers/Api.php
+++ admin/application/controllers/Api.php
@@ -9,8 +9,53 @@ class Api extends CI_Controller {
 	}
 	
 	function get_top_category(){
-		$response = array();
 		$res = $this->APi_Model->get_top_categories();
+		echo json_encode($res);
+	}
+
+	function get_active_products(){
+		$res = $this->APi_Model->get_active_products();
+		echo json_encode($res);
+	}
+
+	function get_active_products_by_category(){
+		if(isset($_GET['category']))
+			$category = $_GET['category'];
+		else
+		$category = 0;
+
+		$res = $this->APi_Model->get_active_products_by_category($category);
+		echo json_encode($res);
+	}
+
+	function get_active_products_by_search(){
+
+		$postdata = file_get_contents("php://input");
+		$request = json_decode($postdata,true);
+		header('Content-type: application/json');
+		header("Access-Control-Allow-Origin: *");
+		header('Access-Control-Allow-Headers: X-Requested-With, content-type, access-control-allow-origin, access-control-allow-methods, access-control-allow-headers');
+
+		if($request['Name'])
+			$name = $request['Name'];
+		else
+			$name = '';
+
+		$priceFrom = $request['PriceFrom'];
+		$priceTo = $request['PriceTo'];
+
+		$res = $this->APi_Model->get_active_products_by_search($name,$priceFrom,$priceTo);
+		echo json_encode($res);
+
+	}
+
+	function get_active_product_by_id(){
+		if(isset($_GET['Id']))
+			$id = $_GET['Id'];
+		else
+			$id = 0;
+
+		$res = $this->APi_Model->get_active_product_by_id($id);
 		echo json_encode($res);
 	}
 }
--- frontend/src/app/components/products/products.component.ts
+++ frontend/src/app/components/products/products.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductService } from 'src/app/services/product.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-products',
@@ -9,14 +11,78 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductsComponent implements OnInit {
 
   public Heading = 'All Products';
+  public Products: Array<Product> = [];
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private productService: ProductService
   ) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
-      this.Heading = params.get('category').toString();
+      if (params.get('category')) {
+        this.Products = [];
+        this.Heading = params.get('category').toString();
+        this.productService.getActiveProductListByCategory(this.Heading).subscribe( res => {
+          res.forEach( element => {
+            let el: Product = {
+              Id: element.Id,
+              Name: element.Name,
+              Price: element.Price,
+              SubCategory: element.SubCategory,
+              Image: environment.BASE_URL + '/' + element.Image,
+              Status: element.Status,
+              CreateAt: element.CreateAt,
+              ModifiedAt: element.ModifiedAt
+            };
+            this.Products.push(el);
+          });
+        });
+      } else {
+        this.Products = [];
+        this.Heading = 'All Products';
+        this.productService.getActiveProductList().subscribe( res => {
+          res.forEach( element => {
+            let el: Product = {
+              Id: element.Id,
+              Name: element.Name,
+              Price: element.Price,
+              SubCategory: element.SubCategory,
+              Image: environment.BASE_URL + '/' + element.Image,
+              Status: element.Status,
+              CreateAt: element.CreateAt,
+              ModifiedAt: element.ModifiedAt
+            };
+            this.Products.push(el);
+          });
+        });
+      }
+
+    });
+  }
+
+  onSearch(event) {
+    const searchData: any = {
+      Name: event,
+      PriceFrom: 0,
+      PriceTo: 0
+    };
+    this.productService.getActiveProductsBySearch(searchData).subscribe( res => {
+      this.Products = [];
+      console.log(res);
+      res.forEach( element => {
+        let el: Product = {
+          Id: element.Id,
+          Name: element.Name,
+          Price: element.Price,
+          SubCategory: element.SubCategory,
+          Image: environment.BASE_URL + '/' + element.Image,
+          Status: element.Status,
+          CreateAt: element.CreateAt,
+          ModifiedAt: element.ModifiedAt
+        };
+        this.Products.push(el);
+      });
     });
   }
 
