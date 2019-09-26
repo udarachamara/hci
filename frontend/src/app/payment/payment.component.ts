90959982f28dc68041b162454aca1a9c604368a3
aathif96 <49545347+aathif96@users.noreply.github.com>
2019-09-26 21:25:47 +0530

    payment

--- frontend/src/app/payment/payment.component.ts
+++ frontend/src/app/payment/payment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-payment',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent implements OnInit {
+  registerForm: FormGroup;
+    submitted = false;
 
-  constructor() { }
+
+    constructor(private formBuilder: FormBuilder) { }
+
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      address: ['', Validators.required],
+      contact: ['', Validators.required,Validators.pattern("^[0-9]*$"),Validators.minLength(10)]
+    });
+      
   }
+  get f() { return this.registerForm.controls; }
+  onSubmit() {
+    this.submitted = true;
 
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      
+        return;
+    }
+
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
 }
+
+onReset() {
+        this.submitted = false;
+        this.registerForm.reset();
+    }
+  }
